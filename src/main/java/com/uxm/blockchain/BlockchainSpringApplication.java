aacce25e48f7f64332500242f5ae104d5961e330
youngcircle-kim <dudghks021121@gmail.com>
2024-01-06 11:00:06 +0900

    first commit

---
+++ src/main/java/com/uxm/blockchain/BlockchainSpringApplication.java
@@ -0,0 +1,13 @@
+package com.uxm.blockchain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlockchainSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlockchainSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/uxm/blockchain/BlockchainSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uxm.blockchain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlockchainSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
